9782bf4dac695007e26ebe126ebedf9cd075d617
HyScript7 <hyscript7@seznam.cz>
2022-09-24 12:16:57 +0200

    Commiting Changes Reorganising files

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@
 #  \ \_\    \ \__|    \ \__/".~\_\  \ \_____\  \ \_____\ 
 #   \/_/     \/_/      \/_/   \/_/   \/_____/   \/_____/ 
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from decouple import config
 
 app = Flask(__name__)
@@ -16,12 +16,13 @@ def index():
 
 @app.route('/dev')
 def dev():
-    return render_template("index.html")
+    temp = request.args.get("template", "index")
+    return render_template(f"{temp}.html")
 
 if __name__ == '__main__':
     from sys import exit
     #* env = config('ENV_VARIABLE', cast='TYPE')
     # Get debug env
-    debug = len(str(config('FLASK_DEBUG')))!=5 # I am too lazy to fuck around with types
-    app.run(host='127.0.0.1', port=8000, debug=debug)
-
+    debug = bool(config('FLASK_DEBUG'))
+    app.run(host='0.0.0.0', port=5000, debug=debug)
+    exit()
