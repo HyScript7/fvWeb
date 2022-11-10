2f81b7f399333d860d46a82b73b17e3e7c3bfd18
HyScript7 <hyscript7@seznam.cz>
2022-11-10 16:17:43 +0100

    Update Bootstrap to v5.2.2

--- app/static/sass/getBootstrap.ps1
+++ app/static/sass/getBootstrap.ps1
@@ -1,9 +1,9 @@
-Invoke-WebRequest -Method Get -Uri "https://github.com/twbs/bootstrap/archive/v5.0.2.zip" -OutFile ".\v5.0.2.zip"
-Expand-Archive -LiteralPath .\v5.0.2.zip -DestinationPath .\
+Invoke-WebRequest -Method Get -Uri "https://github.com/twbs/bootstrap/archive/v5.2.2.zip" -OutFile ".\v5.2.2.zip"
+Expand-Archive -LiteralPath .\v5.2.2.zip -DestinationPath .\
 New-Item -Path '.\bootstrap' -ItemType Directory
-Move-Item ".\bootstrap-5.0.2\scss" ".\bootstrap\scss"
-Remove-Item -r -Force ".\bootstrap-5.0.2\"
-Remove-Item ".\v5.0.2.zip"
+Move-Item ".\bootstrap-5.2.2\scss" ".\bootstrap\scss"
+Remove-Item -r -Force ".\bootstrap-5.2.2\"
+Remove-Item ".\v5.2.2.zip"
 Invoke-WebRequest -Method Get -Uri "https://github.com/twbs/icons/releases/download/v1.9.1/bootstrap-icons-1.9.1.zip" -OutFile ".\bootstrap-icons-1.9.1.zip"
 Expand-Archive -LiteralPath .\bootstrap-icons-1.9.1.zip -DestinationPath .\
 New-Item -Path '..\css\icons' -ItemType Directory
--- app/static/sass/getBootstrap.sh
+++ app/static/sass/getBootstrap.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 # Run this script to download boostrap scss, this is required to run the server without using docker.
-wget https://github.com/twbs/bootstrap/archive/v5.0.2.zip
-unzip ./v5.0.2.zip
+wget https://github.com/twbs/bootstrap/archive/v5.2.2.zip
+unzip ./v5.2.2.zip
 mkdir bootstrap
-mv ./bootstrap-5.0.2/scss ./bootstrap/scss
-rm -frd ./bootstrap-5.0.2
-rm ./v5.0.2.zip
+mv ./bootstrap-5.2.2/scss ./bootstrap/scss
+rm -frd ./bootstrap-5.2.2
+rm ./v5.2.2.zip
 wget https://github.com/twbs/icons/releases/download/v1.9.1/bootstrap-icons-1.9.1.zip
 unzip ./bootstrap-icons-1.9.1.zip
 mkdir ../css/icons
