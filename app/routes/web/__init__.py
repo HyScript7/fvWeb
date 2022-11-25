29133c1d840a7b35f964b350a62bd42a4b6c1c84
HyScript7 <hyscript7@seznam.cz>
2022-11-25 15:55:26 +0100

    Hot fix + Bump version number

--- app/routes/web/__init__.py
+++ app/routes/web/__init__.py
@@ -1,3 +1,15 @@
+#  ______   __   __   __     __     ______     ______    
+# /\  ___\ /\ \ / /  /\ \  _ \ \   /\  ___\   /\  == \   
+# \ \  __\ \ \ \'/   \ \ \/ ".\ \  \ \  __\   \ \  __<   
+#  \ \_\    \ \__|    \ \__/".~\_\  \ \_____\  \ \_____\ 
+#   \/_/     \/_/      \/_/   \/_/   \/_____/   \/_____/ 
+#
+# fvWeb
+# Version: 2.2
+# Author(s): HyScript7
+# License: MIT LICENSE
+# For more information on copyright and licensing view the README.md file.
+#
 from flask import Blueprint
 from decouple import config
 from pymongo import MongoClient
