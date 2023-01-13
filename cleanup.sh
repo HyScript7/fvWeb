5cbe6bd3b317640f595b4f0b8db566540ec8ece3
HyScript7 <hyscript7@seznam.cz>
2023-01-13 13:18:37 +0100

    Minor changes - Edit gitignore - Remove crappy cleanup script

--- cleanup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-rm -frd ./app/__pycache__/
-rm -frd ./app/routes/api/__pycache__/
-rm -frd ./app/routes/web/__pycache__/
-rm -frd ./app/routes/web/forum/__pycache__/
-rm -frd ./app/routes/web/wiki/__pycache__/
-rm -frd ./app/routes/web/home/__pycache__/
-echo Deleted all __pycache__ folders!
