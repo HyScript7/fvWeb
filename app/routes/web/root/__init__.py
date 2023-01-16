cd48f914541999a592a2610ee08bbd244826269f
HyScript7 <hyscript7@seznam.cz>
2023-01-16 17:16:04 +0100

    Favicon, static images

--- app/routes/web/root/__init__.py
+++ app/routes/web/root/__init__.py
@@ -1,6 +1,6 @@
 from common.route_vars import css, js, navbar
 from common.configuration import HOSTNAME
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, send_file
 
 web = Blueprint("root", __name__)
 
@@ -10,3 +10,8 @@ async def root():
     return render_template(
         "root/index.html", page="Home", title="", css=css, js=js, navbar=navbar, hostname=HOSTNAME
     )
+
+
+@web.route("/favicon.ico")
+async def favicon():
+    return send_file("./static/img/favicon.ico")
