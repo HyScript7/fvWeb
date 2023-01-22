5fc1ce854a0e1738180c13bbc51e4f25bbfd8f2a
HyScript7 <hyscript7@seznam.cz>
2023-01-22 13:59:32 +0100

    Authentication Front End Placeholder

--- app/routes/web/root/__init__.py
+++ app/routes/web/root/__init__.py
@@ -1,6 +1,6 @@
 from common.route_vars import css, js, navbar
 from common.configuration import HOSTNAME
-from flask import Blueprint, render_template, send_file
+from flask import Blueprint, render_template, send_file, redirect
 
 web = Blueprint("root", __name__)
 
@@ -9,6 +9,23 @@ web = Blueprint("root", __name__)
 async def root():
     return render_template(
         "root/index.html", page="Home", title="", css=css, js=js, navbar=navbar, hostname=HOSTNAME
+    )
+
+@web.route("/auth")
+async def authentication_redirect():
+    return redirect("/auth/login")
+
+
+@web.route("/auth/<request>")
+async def authentication(request: str):
+    if request.lower() == "login":
+        request = "login"
+    elif request.lower() == "register":
+        request = "register"
+    else:
+        request = "login"
+    return render_template(
+        "root/auth.html", page="Auth", title=request, css=css, js=js, navbar=navbar, menu=request
     )
 
 
