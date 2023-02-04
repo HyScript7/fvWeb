9c2bfbed8d1f1c38d974aa8d1b6f7b26e35f0181
HyScript7 <hyscript7@seznam.cz>
2023-02-04 21:21:01 +0100

    Add Quill into dependencies

--- app/getDependencies.sh
+++ app/getDependencies.sh
@@ -4,7 +4,11 @@ echo Downloading and installing CSS \& JS libraries and dependencies.
 wget https://unpkg.com/@popperjs/core@2.11.6/dist/umd/popper.min.js
 wget https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.min.js
 wget https://code.jquery.com/jquery-3.6.1.min.js
+wget https://cdn.quilljs.com/1.3.6/quill.min.js
 mv ./*.min.js ./static/js/
+
+wget https://cdn.quilljs.com/1.3.6/quill.snow.css
+mv ./quill.snow.css ./static/css/
 
 wget https://github.com/twbs/bootstrap/archive/v5.2.2.zip
 unzip ./v5.2.2.zip
@@ -21,4 +25,3 @@ rm -frd ./bootstrap-icons-1.9.1
 rm ./bootstrap-icons-1.9.1.zip
 
 echo Done!
-
