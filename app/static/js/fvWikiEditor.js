d7673d717d1ce8c1a153b51109dd95a9c49fad7a
HyScript7 <hyscript7@seznam.cz>
2023-02-04 21:19:44 +0100

    Add script for the wiki editor

---
+++ app/static/js/fvWikiEditor.js
@@ -0,0 +1,19 @@
+var editor = new Quill("#editor", {
+  modules: { toolbar: "#toolbar" },
+  theme: "snow",
+});
+$(document).ready(function () {
+  $("#editor-controls").on("submit", function () {
+    console.log("Submiting!");
+    var hvalue = $(".ql-editor").html();
+    hvalue = hvalue.replace(/></g, ">\n<");
+    $(this).append(
+      "<textarea name='content' style='display:none'>" + hvalue + "</textarea>"
+    );
+  });
+  // reset
+  $("#editor-controls").on("reset", function () {
+    console.log("Resetting!");
+    editor.setContents([{ insert: "\n" }]);
+  });
+});
